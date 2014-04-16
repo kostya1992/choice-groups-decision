0c137c2c064eddef381a9f3339c6b38c4dcefa71
vatslavska <vatslavska@gmail.com>
2014-04-16 18:40:24 +0000

    Add jquery-ui for sortable list. Read values from sortable list for each experts in double array.

--- scripts.js
+++ scripts.js
@@ -5,3 +5,23 @@
 function showMessage(){
 alert("Все ОК!");
 }
+//function for sort list
+$(function() {
+    $(".sortable").sortable();
+});
+
+//process by all methods  
+function process(){
+	var experts=document.getElementsByName("expert");
+	var alternatives = [];
+	for(i=0;i<experts.length;i++){
+		alternatives[i]=[];
+		var childNodes=experts[i].getElementsByTagName('li');
+		for(j=0;j<childNodes.length;j++){
+				alternatives[i][j]=childNodes[j].id;
+			}
+	}
+	alert(alternatives);
+	//TODO: add invocation of all methods
+	
+}
