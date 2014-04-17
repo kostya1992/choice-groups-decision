759e9d8b825379e60677237979e7e8de283c7db7
vatslavska@gmail.com <vatslavska@gmail.com@cec6ccaf-b36e-8b10-6e1e-5003d7a57808>
2014-04-17 16:03:04 +0000

    Added method Borda

--- trunk/scripts.js
+++ trunk/scripts.js
@@ -2,52 +2,83 @@
  * 
  */
 
-function showMessage(){
-alert("Hello!");
+function showMessage() {
+	alert("Hello!");
 }
-//function for sort list
+// function for sort list
 $(function() {
-    $(".sortable").sortable();
+	$(".sortable").sortable();
 });
 
-//process by all methods  
-function process(){
-	var experts=document.getElementsByName("expert");
+// process by all methods
+function process() {
+	var experts = document.getElementsByName("alternatives");
 	var alternatives = [];
-	var childNodes=[];
-	for(var i=0;i<experts.length;i++){
-		alternatives[i]=[];
-		childNodes=experts[i].getElementsByTagName('li');
-		for(var j=0;j<childNodes.length;j++){
-				alternatives[i][childNodes[j].id]=childNodes[j].innerText;
+	var childNodes = [];
+	for (var i = 0; i < experts.length; i++) {
+		alternatives[i] = [];
+		childNodes = experts[i].getElementsByTagName('li');
+		for (var j = 0; j < childNodes.length; j++) {
+			alternatives[i][childNodes[j].id] = childNodes[j].innerText;
 		}
 	}
-	//result in double array[i][j]. i - methods, j - result by method 
-	var result = getAllResults(alternatives, childNodes);
-	alert(result);
+	// result in double array[i][j]. i - methods, j - result by method
+	var result = getAllResults(alternatives);
+	var s='';
+	for (var i = 0; i < result.length; i++) {
+		for (key in result[i]) {
+			s+=key+' '+result[i][key]+" | ";
+		}
+	}
+	
+	alert(s);
 }
 
-function getAllResults(alternatives,childNodes){
+function getAllResults(alternatives) {
 	var resultAllMethods = [];
 	var bordaResult = getBordaResult(alternatives);
-	resultAllMethods=convertResult(resultAllMethods, 0, bordaResult, childNodes);
+	resultAllMethods = convertResult(resultAllMethods, 0, bordaResult,
+			alternatives);
 	var koplendaResult = getKoplendaResult(alternatives);
-	resultAllMethods=convertResult(resultAllMethods, 1, koplendaResult, childNodes);
-	//add all methods
+	resultAllMethods = convertResult(resultAllMethods, 1, koplendaResult,
+			alternatives);
+	// add all methods
 	return resultAllMethods;
 }
-function convertResult(resultAllMethods,i,result,childNodes){
-	resultAllMethods[i]=[];
-	for (key in result) {
-		resultAllMethods[i].push(childNodes[key].innerText);
+function convertResult(resultAllMethods, i, result, alternatives) {
+	resultAllMethods[i] = [];
+	if (alternatives.length > 0) {
+		for (key in result) {
+			resultAllMethods[i].push(alternatives[0][key]);
+		}
 	}
 	return resultAllMethods;
 }
-function getBordaResult(alternatives){
-	//TODO implement Method Borda
+function getBordaResult(alternatives) {
+	var relultMark = [];
+	var expertCount = alternatives.length;
+
+	for (var i = 0; i < expertCount; i++) {
+		var altCount = 1;
+		var alternativeCount = arraySize(alternatives[i]);
+		for (key in alternatives[i]) {
+			if (!relultMark.hasOwnProperty(key)) {
+				relultMark[key] = 0;
+			}
+			relultMark[key] += (alternativeCount - altCount);
+			altCount++;
+		}
+	}
+	return relultMark;
+}
+function getKoplendaResult(alternatives) {
+	// TODO implement Method Koplenda
 	return alternatives[0];
 }
-function getKoplendaResult(alternatives){
-	//TODO implement Method Koplenda
-	return alternatives[0];
-}
+function arraySize(obj) {
+	var size = 0;
+	for (key in obj) {
+		size++;
+	}
+	return size;
+};
