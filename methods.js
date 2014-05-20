174c081fd596862f80ec6845bbc091831242b2d7
kostyabaranov92 <kostyabaranov92@gmail.com>
2014-05-20 13:26:49 +0000

    add simpson method

--- methods.js
+++ methods.js
@@ -16,20 +16,21 @@ function process() {
 	var s = '';
 	for (var i = 0; i < result.length; i++) {
 		for (key in result[i]) {
-			s += key + ' - ' + result[i][key][0] +" - "+result[i][key][1]+ " | ";
+			s += key + ' - ' + result[i][key][0] + " - " + result[i][key][1]
+					+ " | ";
 		}
-		s+="\n";
+		s += "\n";
 	}
-    alert(s);
+	alert(s);
 	console.log(s);
 }
 
 function getAllResults(alternatives) {
 	var resultAllMethods = [];
 	var bordaResult = getBordaResult(alternatives);
-	resultAllMethods[0] =convertResult(bordaResult, alternatives);// sortAssosiationArrayDesc(bordaResult);
-	//var kondorseResult = getKondorseResult(alternatives);
-//	 resultAllMethods[1] = sortAssosiationArrayDesc(kondorseResult);
+	resultAllMethods[0] = convertResult(bordaResult, alternatives);// sortAssosiationArrayDesc(bordaResult);
+	// var kondorseResult = getKondorseResult(alternatives);
+	// resultAllMethods[1] = sortAssosiationArrayDesc(kondorseResult);
 	// add all methods
 	return resultAllMethods;
 }
@@ -38,7 +39,7 @@ function convertResult(result, alternatives) {
 	if (alternatives.length > 0) {
 		var sortResult = sortAssosiationArrayDesc(result);
 		for (key in sortResult) {
-			resultMethod.push([alternatives[0][key],sortResult[key]]);
+			resultMethod.push([ alternatives[0][key], sortResult[key] ]);
 		}
 	}
 	return resultMethod;
@@ -62,63 +63,85 @@ function getKoplendaResult(alternatives) {
 	// TODO implement Method Koplenda
 	return alternatives[0];
 }
+
+function getSimpsonsResult(alternatives) {
+	var pairs = [];
+	makeSimpsonPairs(alternatives[0], pairs);
+	alert(JSON.stringify(pairs));
+}
+
+function makeSimpsonPairs(someArray, storage) {
+	for (var i = 0; i < someArray.length; i++) {
+		for (var j = 0; j < someArray.length; j++) {
+			storage.push([ someArray[i], someArray[j] ]);
+		}
+	}
+	for (var i = 0; i < storage.length; i++) {
+		var first = storage[i];
+		if(first[0] == first[1]){
+			storage.splice(i, 1);
+		}
+	}
+}
+
 function getKondorseResult(alternatives) {
-    var pairs = [];
-    makePairs(alternatives[0], pairs);
-    console.log("all possible pairs - " + JSON.stringify(pairs));
-    var comparisonResult = [];
-    var comparisonArr = [];
-    _.each(pairs, function (pair) {
-        var comparedGroup = compare(pair[0], pair[1], alternatives);
-        if(!_.has(comparedGroup,pair[0])){
-            var obj = {};
-            obj[pair[0]]=0;
-            _.extend(comparedGroup,obj);
-        }
-        if(!_.has(comparedGroup,pair[1])){
-            var obj = {};
-            obj[pair[1]]=0;
-            _.extend(comparedGroup,obj);
-        }
-        comparisonResult.push(comparedGroup);
-        var sortedPairs = _.sortBy(_.pairs(comparedGroup), function (pair) {
-            return -1 * pair[1];
-        });
-        comparisonArr.push([sortedPairs[0][0], sortedPairs[1][0]]);
-    });
-    console.log("Comparison result by Kondorse : " + JSON.stringify(comparisonResult));
-    var rating = [];
-    rating.push(comparisonArr[0][0]);
-    rating.push(comparisonArr[0][1]);
-    kondorseBackward(_.rest(comparisonArr), comparisonArr[0], rating);
-    kondorseForward(_.rest(comparisonArr), comparisonArr[0], rating);
-    alert("The winner is " + $("#"+rating[0]+" input").val());
+	var pairs = [];
+	makePairs(alternatives[0], pairs);
+	console.log("all possible pairs - " + JSON.stringify(pairs));
+	var comparisonResult = [];
+	var comparisonArr = [];
+	_.each(pairs, function(pair) {
+		var comparedGroup = compare(pair[0], pair[1], alternatives);
+		if (!_.has(comparedGroup, pair[0])) {
+			var obj = {};
+			obj[pair[0]] = 0;
+			_.extend(comparedGroup, obj);
+		}
+		if (!_.has(comparedGroup, pair[1])) {
+			var obj = {};
+			obj[pair[1]] = 0;
+			_.extend(comparedGroup, obj);
+		}
+		comparisonResult.push(comparedGroup);
+		var sortedPairs = _.sortBy(_.pairs(comparedGroup), function(pair) {
+			return -1 * pair[1];
+		});
+		comparisonArr.push([ sortedPairs[0][0], sortedPairs[1][0] ]);
+	});
+	console.log("Comparison result by Kondorse : "
+			+ JSON.stringify(comparisonResult));
+	var rating = [];
+	rating.push(comparisonArr[0][0]);
+	rating.push(comparisonArr[0][1]);
+	kondorseBackward(_.rest(comparisonArr), comparisonArr[0], rating);
+	kondorseForward(_.rest(comparisonArr), comparisonArr[0], rating);
+	alert("The winner is " + $("#" + rating[0] + " input").val());
 }
 
 function kondorseBackward(comparisonArr, pair, rating) {
-//    console.log("backward =" + JSON.stringify(comparisonArr));
-    var first = pair[0];
-    var betterPair = _.find(comparisonArr, function (value) {
-        return value[1] == first;
-    });
-    if (betterPair != undefined) {
-        rating.splice(_.indexOf(rating, first), 0, betterPair[0]);
-        var rest = _.without(comparisonArr, betterPair);
-        kondorseBackward(rest, betterPair, rating);
-    }
+	// console.log("backward =" + JSON.stringify(comparisonArr));
+	var first = pair[0];
+	var betterPair = _.find(comparisonArr, function(value) {
+		return value[1] == first;
+	});
+	if (betterPair != undefined) {
+		rating.splice(_.indexOf(rating, first), 0, betterPair[0]);
+		var rest = _.without(comparisonArr, betterPair);
+		kondorseBackward(rest, betterPair, rating);
+	}
 }
 
 function kondorseForward(comparisonArr, pair, rating) {
-//    console.log("forward=" + JSON.stringify(comparisonArr));
-    var last = pair[1];
-    var worsePair = _.find(comparisonArr, function (value) {
-        return value[0] == last;
-    });
-    if (worsePair != undefined) {
-        rating.push(worsePair[1]);
-        var rest = _.without(comparisonArr, worsePair);
-        kondorseForward(rest, worsePair, rating);
-    }
+	// console.log("forward=" + JSON.stringify(comparisonArr));
+	var last = pair[1];
+	var worsePair = _.find(comparisonArr, function(value) {
+		return value[0] == last;
+	});
+	if (worsePair != undefined) {
+		rating.push(worsePair[1]);
+		var rest = _.without(comparisonArr, worsePair);
+		kondorseForward(rest, worsePair, rating);
+	}
 }
 
 function arraySize(arr) {
@@ -143,72 +166,121 @@ function sortAssosiationArrayDesc(arr) {
 	return sortedObj;
 };
 
-
 function compare(alt1, alt2, results) {
-    return _.countBy(results, function (vote) {
-        return _.indexOf(vote,alt1) <= _.indexOf(vote,alt2) ? alt1 : alt2;
-    });
+	return _.countBy(results, function(vote) {
+		return _.indexOf(vote, alt1) <= _.indexOf(vote, alt2) ? alt1 : alt2;
+	});
 }
 
 function makePairs(someArray, storage) {
-    var rest = _.rest(someArray, 1);
-    for (var i = 0; i < rest.length; i++) {
-        storage.push([ someArray[0], rest[i] ]);
-    }
-    if (rest.length != 0)
-        makePairs(rest, storage);
+	var rest = _.rest(someArray, 1);
+	for (var i = 0; i < rest.length; i++) {
+		storage.push([ someArray[0], rest[i] ]);
+	}
+	if (rest.length != 0)
+		makePairs(rest, storage);
 }
-
-$(function () {
-    $("#kondorse").on("click", function () {
-        var voteResults = [];
-        if ($(".experts") != undefined) {
-            $(".experts > li").each(function (index) {
-                var alternativesSelector = "#alternativesOfEpert" + index + " li";
-                var alternativesList = [];
-                $(".experts").find(alternativesSelector).each(function () {
-                    alternativesList.push($(this).attr("id"));
-                })
-                voteResults.push(alternativesList);
-            })
-        }
-        console.log(JSON.stringify(voteResults));
-        if (voteResults.length != 0) {
-            getKondorseResult(voteResults);
-        } else {
-            alert("Vote results aren't specified");
-        }
-    });
+$(function() {
+	$("#simpson")
+			.on(
+					"click",
+					function() {
+						var voteResults = [];
+						if ($(".experts") != undefined) {
+							$(".experts > li")
+									.each(
+											function(index) {
+												var alternativesSelector = "#alternativesOfEpert"
+														+ index + " li";
+												var alternativesList = [];
+												$(".experts")
+														.find(
+																alternativesSelector)
+														.each(
+																function() {
+																	alternativesList
+																			.push($(
+																					this)
+																					.attr(
+																							"id"));
+																})
+												voteResults
+														.push(alternativesList);
+											})
+						}
+						console.log(JSON.stringify(voteResults));
+						if (voteResults.length != 0) {
+							getSimpsonsResult(voteResults);
+						} else {
+							alert("Vote results aren't specified");
+						}
+					});
 });
-$(function () {
+
+$(function() {
+	$("#kondorse")
+			.on(
+					"click",
+					function() {
+						var voteResults = [];
+						if ($(".experts") != undefined) {
+							$(".experts > li")
+									.each(
+											function(index) {
+												var alternativesSelector = "#alternativesOfEpert"
+														+ index + " li";
+												var alternativesList = [];
+												$(".experts")
+														.find(
+																alternativesSelector)
+														.each(
+																function() {
+																	alternativesList
+																			.push($(
+																					this)
+																					.attr(
+																							"id"));
+																})
+												voteResults
+														.push(alternativesList);
+											})
+						}
+						console.log(JSON.stringify(voteResults));
+						if (voteResults.length != 0) {
+							getKondorseResult(voteResults);
+						} else {
+							alert("Vote results aren't specified");
+						}
+					});
+});
+$(function() {
 	$("#borda").on("click", function() {
 		var voteResults = [ {
-			alternative_0 :"0",
-			alternative_2: "2",
-			alternative_1 :"1"
+			alternative_0 : "0",
+			alternative_2 : "2",
+			alternative_1 : "1"
 		}, {
-			alternative_1 :"1",
-			alternative_0: "0",
-			alternative_2 :"2"
+			alternative_1 : "1",
+			alternative_0 : "0",
+			alternative_2 : "2"
 		}, {
-			alternative_2 :"2",
-			alternative_1: "1",
-			alternative_0 :"0"
+			alternative_2 : "2",
+			alternative_1 : "1",
+			alternative_0 : "0"
 		}, {
-			alternative_0 :"0",
-			alternative_1: "1",
-			alternative_2 :"2"
-		}];
-		var result=getKonsorseResult(voteResults);
+			alternative_0 : "0",
+			alternative_1 : "1",
+			alternative_2 : "2"
+		} ];
+		var result = getKonsorseResult(voteResults);
 		console.log(result);
-				var sort=sortAssosiationArrayDesc(result);
-					console.log(sort);
-					console.log("borda:");
+		var sort = sortAssosiationArrayDesc(result);
+		console.log(sort);
+		console.log("borda:");
 
-		var result=getBordaResult(voteResults);
-console.log(result);
-		var sort=sortAssosiationArrayDesc(result);
-			console.log(sort);
+		var result = getBordaResult(voteResults);
+		console.log(result);
+		var sort = sortAssosiationArrayDesc(result);
+		console.log(sort);
 	});
 });
-
